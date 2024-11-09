9e5fbb45e33e859c2213986a5a386bb0f632d790
Gold_Ch <54826508+GChan-Ch@users.noreply.github.com>
2024-11-09 20:55:12 +0700

    Update Backup_dan_Kompresi_File.sh

--- LINUX/Backup_dan_Kompresi_File.sh
+++ LINUX/Backup_dan_Kompresi_File.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
-
-if [ "$#" -ne 2 ]; then
-  echo "Penggunaan: $0 <direktori_sumber> <lokasi_backup>"
-  exit 1
-fi
-
-direktori_sumber=$1
-lokasi_backup=$2
-
-if [ ! -d "$direktori_sumber" ]; then
-  echo "Direktori sumber $direktori_sumber tidak ditemukan."
-  exit 1
-fi
-
-nama_backup="backup_$(basename "$direktori_sumber").tar.gz"
-echo "Mem-backup direktori $direktori_sumber ke $lokasi_backup/$nama_backup"
-tar -czf "$lokasi_backup/$nama_backup" -C "$direktori_sumber" .
-
-if [ $? -eq 0 ]; then
-  echo "Backup berhasil disimpan di $lokasi_backup/$nama_backup"
-else
-  echo "Backup gagal."
-  exit 1
-fi
+backup="/home/folder"
+dest="/mnt/backup"
+day=$(date +%A)
+hostname=$(hostname -s)
+archive="$hostname-$day.tgz
+tar czf $dest/$archive $backup
+echo "Backup finished"
